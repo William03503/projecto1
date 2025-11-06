588c07f9b1a4cecb61652db21284074ec2c8b36a
Usu2 name <william03503@gmail.com>
2025-11-07 00:02:02 +0100

    Añado el paquete cambio y la clase cambio

---
+++ proyecto1/src/cambio1/Cambio1.java
@@ -0,0 +1,16 @@
+package cambio1;
+
+public class Cambio1 {
+	public static void main(String[] args) {
+		System.out.println("Pruebas Git. Clase Cambio1");
+	}
+}
+class Principal{
+	public static void main(String[] args) {
+		System.out.println("Hola mundo java-git");
+		System.out.println("Realizo el primer cambio. Añado una visuaizacion");
+		System.out.println("Realizo el segundo cambio. Añado una visualizacion");
+		System.out.println("Pruebas Git. Se añade el cambio");
+	}
+	
+}
